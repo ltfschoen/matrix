f509dc9dcb8529d3610ae15b342a1ac622dd7095
Luke Schoen <ltfschoen@gmail.com>
2019-04-21 00:41:29 +0200

    refactor: Refactor similar to polkadobot code

--- src/index.js
+++ src/index.js
@@ -1,82 +1,113 @@
 
+import EventEmitter from 'events';
 import { config as dotenvConfig } from 'dotenv';
 import sdk from 'matrix-js-sdk';
 
-import Bot from './methods';
+import {
+  handleNewMember
+} from './methods/eventHandlers';
 
 dotenvConfig();
 
-const client = sdk.createClient(process.env.BASE_URL);
+let hasCalledInitBot = false;
 
-client.publicRooms((err, data) => {
-  if (err) return console.log('Error loading public rooms', err);
+class BotApp extends EventEmitter {
+  constructor (options) {
+    super();
 
-  console.log(`Public Rooms: ${JSON.stringify(data)}`);
-});
-
-client.login(
-  'm.login.password',
-  {
-    user: process.env.MATRIX_USERNAME,
-    password: process.env.MATRIX_PASSWORD,
-  },
-  (err, data) => {
-    if (err) { console.log('Error logging into matrix:', err); }
-    console.log(`Logged in ${data.user_id} on device ${data.device_id}`);
-
-    client.startClient(0);
-    console.log('Started client');
-
-    /**
-     * Event listeners for message events emitted from a room's timeline
-     */
-    client.on('Room.timeline', function(event, room, toStartOfTimeline) {
-      if (event.getType() !== 'm.room.message') {
-        return; // only use messages
-      }
-
-      console.log('Received event: ', event.event);
-      console.log('Received event sender: ', event.sender);
-
-      const options = {};
-
-      const bot = new Bot(options);
-
-      const privateRooms = {};
-
-      bot.handleNewMember(
-        event,
-        room,
-        toStartOfTimeline,
-        client,
-        privateRooms
+    if (hasCalledInitBot) {
+      this.emit(
+        'error',
+        new Error('Unable to initialise BotApp more than once')
       );
-    });
+    }
 
-    /**
-     * Wait until sync state is 'PREPARED' before sending messages to the room.
-     */
-    client.once('sync', function(state, prevState, res) {
-      if(state === 'PREPARED') {
-        console.log('Detected that client sync state is prepared.');
-
-        const content = {
-          'body': 'message text',
-          'msgtype': 'm.text'
-        };
-
-        // Hard-code the room id where a bot has been setup for testing in Matrix Riot
-        const roomId = '!KPhFUARUOOHcIXayFS:matrix.parity.io';
-
-        client.sendEvent(roomId, 'm.room.message', content, '', (err, res) => {
-          if (err) { console.log(err) };
-
-          console.log(`Sent message with event id ${res.event_id}`);
-        });
-      } else {
-        console.log('Unable to establish client sync state', state);
-        process.exit(1);
-      }
-    });
+    this.options = options;
   }
-);
+
+  run() {
+    this.matrix = sdk.createClient(process.env.BASE_URL);
+
+    this.matrix.publicRooms((err, data) => {
+      if (err) return console.log('Error loading public rooms', err);
+
+      console.log(`Public Rooms: ${JSON.stringify(data)}`);
+    });
+
+    this.matrix.login(
+      'm.login.password',
+      {
+        user: process.env.MATRIX_USERNAME,
+        password: process.env.MATRIX_PASSWORD,
+      },
+      (err, data) => {
+        const { matrix } = this;
+        const that = this;
+        if (err) { console.log('Error logging into matrix:', err); }
+        console.log(`Logged in ${data.user_id} on device ${data.device_id}`);
+    
+        this.matrix.startClient(0);
+        console.log('Started Matrix client');
+    
+        /**
+         * Event listeners for message events emitted from a room's timeline
+         */
+        matrix.on('Room.timeline', function(event, room, toStartOfTimeline) {
+          if (event.getType() !== 'm.room.message') {
+            return; // only use messages
+          }
+    
+          console.log('Received event: ', event.event);
+          console.log('Received event sender: ', event.sender);
+    
+          const privateRooms = {};
+    
+          that.handleNewMember();
+
+          // bot.handleNewMember(
+          //   event,
+          //   room,
+          //   toStartOfTimeline,
+          //   this.matrix,
+          //   privateRooms
+          // );
+        });
+    
+        /**
+         * Wait until sync state is 'PREPARED' before sending messages to the room.
+         */
+        matrix.once('sync', function(state, prevState, res) {
+          if (state === 'PREPARED') {
+            console.log('Detected that client sync state is prepared.');
+    
+            const content = {
+              'body': 'message text',
+              'msgtype': 'm.text'
+            };
+    
+            // Hard-code the room id where a bot has been setup for testing in Matrix Riot
+            const roomId = '!KPhFUARUOOHcIXayFS:matrix.parity.io';
+    
+            matrix.sendEvent(roomId, 'm.room.message', content, '', (err, res) => {
+              if (err) { console.log(err) };
+    
+              console.log(`Sent message with event id ${res.event_id}`);
+            });
+          } else {
+            console.log('Unable to establish client sync state', state);
+            process.exit(1);
+          }
+        });
+      }
+    );
+  }
+  
+  handleNewMember() {
+    return;
+    // handleNewMember(this);
+  }
+}
+
+var botApp = new BotApp();
+
+botApp.run();
